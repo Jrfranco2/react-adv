dce96c7af30dd8f8e9c79ca8a01aae8d512b4b58
Jonathan Franco <jonas150397@gmail.com>
2022-02-10 11:21:39 -0500

    component patterns

---
+++ src/components-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,35 @@
+import {
+  ProductButtons,
+  ProductCard,
+  ProductImage,
+  ProductTitle,
+} from "../components";
+
+const product = {
+  id: "1",
+  title: "Coffee Mug - Card",
+  img: "./coffee-mug.png",
+};
+
+const ShoppingPage = () => {
+  return (
+    <div>
+      <h1>Shopping Store</h1>
+      <hr />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <ProductCard product={product}>
+          <ProductCard.Image />
+          <ProductCard.Title />
+          <ProductCard.Buttons />
+        </ProductCard>
+        <ProductCard product={product}>
+          <ProductImage />
+          <ProductTitle />
+          <ProductButtons />
+        </ProductCard>
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingPage;
