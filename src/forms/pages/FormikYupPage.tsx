86b757520391628086cae44f194631a92340f02f
Jonathan Franco <jonas150397@gmail.com>
2022-04-08 15:48:46 -0500

    add getFieldProps

--- src/forms/pages/FormikYupPage.tsx
+++ src/forms/pages/FormikYupPage.tsx
@@ -3,26 +3,25 @@ import * as Yup from "yup";
 import "../styles/styles.css";
 
 const FormikYupPage = () => {
-  const { handleChange, values, handleSubmit, errors, touched, handleBlur } =
-    useFormik({
-      initialValues: {
-        firstName: "",
-        lastName: "",
-        email: "",
-      },
-      onSubmit: (values) => {
-        console.log(values);
-      },
-      validationSchema: Yup.object({
-        firstName: Yup.string()
-          .required("Required")
-          .max(15, "Must be 15 characters or less"),
-        lastName: Yup.string()
-          .required("Required")
-          .max(20, "Must be 20 characters or less"),
-        email: Yup.string().required("Required").email("Invalid email address"),
-      }),
-    });
+  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .required("Required")
+        .max(15, "Must be 15 characters or less"),
+      lastName: Yup.string()
+        .required("Required")
+        .max(20, "Must be 20 characters or less"),
+      email: Yup.string().required("Required").email("Invalid email address"),
+    }),
+  });
 
   return (
     <div>
@@ -31,12 +30,9 @@ const FormikYupPage = () => {
         <label htmlFor="firstName">First Name</label>
         <input
           type="text"
-          name="firstName"
           id="firstName"
-          value={values.firstName}
-          onChange={handleChange}
-          onBlur={handleBlur}
           autoComplete="new-text"
+          {...getFieldProps("firstName")}
         />
         {touched.firstName && errors.firstName && (
           <span>{errors.firstName}</span>
@@ -45,24 +41,18 @@ const FormikYupPage = () => {
         <label htmlFor="lastName">Last Name</label>
         <input
           type="text"
-          name="lastName"
           id="lastName"
-          value={values.lastName}
-          onChange={handleChange}
-          onBlur={handleBlur}
           autoComplete="new-text"
+          {...getFieldProps("lastName")}
         />
         {touched.firstName && errors.lastName && <span>{errors.lastName}</span>}
 
         <label htmlFor="email">Email Address</label>
         <input
           type="email"
-          name="email"
           id="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
           autoComplete="new-text"
+          {...getFieldProps("email")}
         />
         {touched.email && errors.email && <span>{errors.email}</span>}
 
